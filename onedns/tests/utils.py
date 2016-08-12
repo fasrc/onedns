7f2a3ca8a8e1ea17fe7749cdb9de5e7bdb9a963d
Justin Riley <justin.t.riley@gmail.com>
2016-08-12 11:03:55 -0400

    add kwargs for server, port, and tcp to dnsquery

--- onedns/tests/utils.py
+++ onedns/tests/utils.py
@@ -3,7 +3,9 @@ import dnslib
 from onedns.tests import conftest
 
 
-def dnsquery(qname, qtype):
+def dnsquery(qname, qtype, server=None, port=None, tcp=False):
+    server = server or conftest.INTERFACE
+    port = port or conftest.PORT
     q = dnslib.DNSRecord(q=dnslib.DNSQuestion(qname, qtype))
-    a_pkt = q.send(conftest.INTERFACE, conftest.PORT, tcp=False)
+    a_pkt = q.send(server, port, tcp=tcp)
     return dnslib.DNSRecord.parse(a_pkt)
